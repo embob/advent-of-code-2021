4fcbb62d4c5b6a62286cf276215c856103befb34
Emma K <emma.karayiannis@gmail.com>
2021-12-29 11:05:36 +0000

    Day 4 part 2 🙌

--- day-4/day-4.js
+++ day-4/day-4.js
@@ -11,6 +11,7 @@ const boardsInput = input
   .map((s) => s.trim().replace(/\s{2}/g, " ").split(" ").map(Number));
 
 const boards = [];
+
 while (boardsInput.length > 0) {
   const board = boardsInput.splice(0, 5);
   boards.push([...board, ...rowToColumns(board)]);
@@ -18,19 +19,35 @@ while (boardsInput.length > 0) {
 
 function removeNumberOnBoard(bingoNumber, board) {
   for (let i = 0; i < board.length; i++) {
-    const matchIndex = board[i].indexOf(bingoNumber);
-    if (matchIndex > -1) {
-      board[i].splice(matchIndex, 1);
+    const line = board[i];
+    const matchedNumberIndex = line.indexOf(bingoNumber);
+    if (matchedNumberIndex > -1) {
+      line.splice(matchedNumberIndex, 1);
     }
   }
 }
 
-function playGame(bingoNumbers, boards) {
+function findFirstWinner(bingoNumbers, boards) {
   for (let i = 0; i < bingoNumbers.length; i++) {
     for (let j = 0; j < boards.length; j++) {
       removeNumberOnBoard(bingoNumbers[i], boards[j]);
       if (boards[j].some((line) => line.length === 0)) {
         return { bingoNumber: bingoNumbers[i], winningBoard: boards[j] };
+      }
+    }
+  }
+}
+
+function findLastWinner(bingoNumbers, boards) {
+  const winningBoards = new Set();
+  for (let i = 0; i < bingoNumbers.length; i++) {
+    for (let j = 0; j < boards.length; j++) {
+      removeNumberOnBoard(bingoNumbers[i], boards[j]);
+      if (boards[j].some((line) => line.length === 0)) {
+        winningBoards.add(boards[j]);
+      }
+      if (winningBoards.size === boards.length) {
+        return { bingoNumber: bingoNumbers[i], winningBoard: boards[j] }
       }
     }
   }
@@ -43,9 +60,10 @@ function calculateBoardSum(board) {
   }, 0);
 };
 
-function calculateScore(bingoNumbers, boards) {
-  const { bingoNumber, winningBoard } = playGame(bingoNumbers, boards);
+function calculateScore(bingoNumbers, boards, findWinner) {
+  const { bingoNumber, winningBoard } = findWinner(bingoNumbers, boards);
   return bingoNumber * calculateBoardSum(winningBoard);
 }
 
-console.log('Part 1 Result →', calculateScore(bingoNumbers, boards));
+console.log('Part 1 Result →', calculateScore(bingoNumbers, boards, findFirstWinner));
+console.log('Part 2 Result →', calculateScore(bingoNumbers, boards, findLastWinner));
